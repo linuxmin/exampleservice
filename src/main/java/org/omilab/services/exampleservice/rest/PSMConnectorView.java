90b714d18b13e09048e24bf73908dd0fa715248f
linuxmin <mistertool1988@gmail.com>
2018-05-28 20:29:00 +0200

    misc changes

--- src/main/java/org/omilab/services/exampleservice/rest/PSMConnectorView.java
+++ src/main/java/org/omilab/services/exampleservice/rest/PSMConnectorView.java
@@ -97,7 +97,7 @@ public final class PSMConnectorView {
 			}
 
         	if(request.getParams().get("navinput").equalsIgnoreCase("forum")){
-        		sb.append(pageBuilder.createThread(forumUser));
+        		//sb.append(pageBuilder.createThread(forumUser));
 				sb.append("<div class=\"list-group\">");
 				for(ForumThread f : forumThreadRepository.findAll()){
 					sb.append(pageBuilder.showForum(f));
