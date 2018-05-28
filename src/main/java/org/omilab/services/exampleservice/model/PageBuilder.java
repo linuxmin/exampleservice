17e2bba50b183b622f2e8dc4a66446120accb3da
linuxmin <mistertool1988@gmail.com>
2018-05-28 09:34:27 +0200

    misc changes

--- src/main/java/org/omilab/services/exampleservice/model/PageBuilder.java
+++ src/main/java/org/omilab/services/exampleservice/model/PageBuilder.java
@@ -8,7 +8,7 @@ public class PageBuilder {
         return "<!DOCTYPE html>\n" +
                 "<html lang=\"en\">\n" +
                 "<head>\n" +
-                "  <title>Bootstrap Example</title>\n" +
+                "  <title>Forum Serrvice</title>\n" +
                 "  <meta charset=\"utf-8\">\n" +
                 "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
                 "  <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css\">\n" +
@@ -19,32 +19,34 @@ public class PageBuilder {
                 "<body>\n" +
                 "\n" +
                 "<div class=\"jumbotron text-center\">\n" +
-                "  <h1>My First Bootstrap Page</h1>\n" +
-                "  <p>Resize this responsive page to see the effect!</p> \n" +
-                "</div>\n" +
-                "  \n" +
-                "<div class=\"container\">\n" +
-                "  <div class=\"row\">\n" +
-                "    <div class=\"col-sm-4\">\n" +
-                "      <h3>Column 1</h3>\n" +
-                "      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>\n" +
-                "      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>\n" +
-                "    </div>\n" +
-                "    <div class=\"col-sm-4\">\n" +
-                "      <h3>Column 2</h3>\n" +
-                "      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>\n" +
-                "      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>\n" +
-                "    </div>\n" +
-                "    <div class=\"col-sm-4\">\n" +
-                "      <h3>Column 3</h3>        \n" +
-                "      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>\n" +
-                "      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>\n" +
-                "    </div>\n" +
-                "  </div>\n" +
-                "</div>\n" +
-                "\n" +
-                "</body>\n" +
-                "</html>\n";
+                "  <h1>Enterprise IS Forum</h1>\n" +
+                "  <p>Discuss the important things of life!</p> \n" +
+                "</div>" + "    <div class=\"row\">\n" +
+                "        <nav class=\"navbar navbar-default navbar-fixed-top\">\n" +
+                "            <div class=\"container-fluid\">\n" +
+                "                <div class=\"navbar-header\">\n" +
+                "                    <a class=\"navbar-brand\" href=\"#\">SKAB - Rent a Car!</a>\n" +
+                "                </div>\n" +
+                "                <div class=\"navbar-default\">\n" +
+                "                    <ul class=\"nav nav-pills\">\n" +
+                "                        <li><a href=\"index.php\">Home</a></li>\n" +
+                "                        <li><a  href=\"fahrzeugsuche.php\">Fahrzeugsuche</a></li>\n" +
+                "                        <li><a  href=\"#\">Flotte</a></li>\n" +
+                "                        <li><a  href=\"administration/admin.html\">Mitarbeiterbereich</a></li>\n" +
+                "                        <form class=\"navbar-form navbar-right\" id=\"loginform\" action=\"index.php\" method=\"post\">\n" +
+                "                            <div class=\"form-group\">\n" +
+                "                                <label for=\"username\">Benutzername</label>\n" +
+                "                                <input type=\"text\" class=\"form-control\" name=\"username\" id=\"username\">\n" +
+                "                                <label for=\"password\">Password</label>\n" +
+                "                                <input type=\"password\" class=\"form-control\" name=\"password\" id=\"password\">\n" +
+                "                            </div>\n" +
+                "                            <button type=\"submit\" formmethod=\"post\" class=\"btn btn-default\">Login</button>\n" +
+                "                            <a href=\"register.html\">Registrierung</a>\n" +
+                "                        </form>\n" +
+                "                </div>\n" +
+                "            </div>\n" +
+                "        </nav>\n" +
+                "    </div>";
 
     }
 
