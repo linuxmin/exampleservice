f08fdf2a03356e872c2475f2f17638f1309ab7c8
linuxmin <mistertool1988@gmail.com>
2018-05-28 09:47:43 +0200

    misc changes

--- src/main/java/org/omilab/services/exampleservice/model/PageBuilder.java
+++ src/main/java/org/omilab/services/exampleservice/model/PageBuilder.java
@@ -21,23 +21,22 @@ public class PageBuilder {
                 "<div class=\"jumbotron text-center\">\n" +
                 "  <h1>Enterprise IS Forum</h1>\n" +
                 "  <p>Discuss the important things of life!</p> \n" +
-                "</div>" + "         <div class=\"navbar navbar-expand-sm bg-dark navbar-dark\">\n" +
-                "                    <ul class=\"nav nav-pills\">\n" +
-                "                        <li><a href=\"index.php\">Home</a></li>\n" +
-                "                        <li><a  href=\"fahrzeugsuche.php\">Fahrzeugsuche</a></li>\n" +
-                "                        <li><a  href=\"#\">Flotte</a></li>\n" +
-                "                        <li><a  href=\"administration/admin.html\">Mitarbeiterbereich</a></li>\n" +
-                "                        <form class=\"navbar-form navbar-right\" id=\"loginform\" action=\"index.php\" method=\"post\">\n" +
-                "                            <div class=\"form-group\">\n" +
-                "                                <label for=\"username\">Benutzername</label>\n" +
-                "                                <input type=\"text\" class=\"form-control\" name=\"username\" id=\"username\">\n" +
-                "                                <label for=\"password\">Password</label>\n" +
-                "                                <input type=\"password\" class=\"form-control\" name=\"password\" id=\"password\">\n" +
-                "                            </div>\n" +
-                "                            <button type=\"submit\" formmethod=\"post\" class=\"btn btn-default\">Login</button>\n" +
-                "                            <a href=\"register.html\">Registrierung</a>\n" +
-                "                        </form>\n" +
-                "                </div>";
+                "<nav class=\"navbar navbar-expand-sm bg-dark navbar-dark\">\n" +
+                "  <ul class=\"navbar-nav\">\n" +
+                "    <li class=\"nav-item active\">\n" +
+                "      <a class=\"nav-link\" href=\"#\">Active</a>\n" +
+                "    </li>\n" +
+                "    <li class=\"nav-item\">\n" +
+                "      <a class=\"nav-link\" href=\"#\">Link</a>\n" +
+                "    </li>\n" +
+                "    <li class=\"nav-item\">\n" +
+                "      <a class=\"nav-link\" href=\"#\">Link</a>\n" +
+                "    </li>\n" +
+                "    <li class=\"nav-item\">\n" +
+                "      <a class=\"nav-link disabled\" href=\"#\">Disabled</a>\n" +
+                "    </li>\n" +
+                "  </ul>\n" +
+                "</nav>";
 
     }
 
