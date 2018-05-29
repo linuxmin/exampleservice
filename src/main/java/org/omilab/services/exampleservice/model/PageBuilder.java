091fb71c72dad55eebf24953be8e899df0216ef0
linuxmin <mistertool1988@gmail.com>
2018-05-29 09:57:23 +0200

    misc changes

--- src/main/java/org/omilab/services/exampleservice/model/PageBuilder.java
+++ src/main/java/org/omilab/services/exampleservice/model/PageBuilder.java
@@ -10,7 +10,8 @@ public class PageBuilder {
         if (!owner) {
             return " <a href=\"javascript:submitform('thread" + forumThread.getThreadId() + "');\" class=\"list-group-item list-group-item-action\">" + forumThread.getThreadTitle() + "<span class=\"badge badge-primary badge-pill\">" + forumThread.getForumPostings().size() + "</span></a>";
         } else {
-            return "<div class=\"list-group-item list-group-item-action\">" +"<a href=\"javascript:submitform('thread" + forumThread.getThreadId() + "');\">" + forumThread.getThreadTitle() + "</a><span class=\"badge badge-primary badge-pill\">" + forumThread.getForumPostings().size() + "</span></div>";
+            return " <a href=\"javascript:submitform('thread" + forumThread.getThreadId() + "');\" class=\"list-group-item list-group-item-action\">" + forumThread.getThreadTitle() + "<span class=\"badge badge-primary badge-pill\">" + forumThread.getForumPostings().size() + "</span>" +
+                    "</a>" + "<div><button type=\"button\" class=\"btn btn-danger\">Danger</button></div>";
 
         }
     }
