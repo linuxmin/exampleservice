584c18ef0c4b50fe3e0bb4ad626d027355842d81
linuxmin <mistertool1988@gmail.com>
2018-05-29 15:37:29 +0200

    misc changes

--- src/main/java/org/omilab/services/exampleservice/repo/ForumPostingRepository.java
+++ src/main/java/org/omilab/services/exampleservice/repo/ForumPostingRepository.java
@@ -2,7 +2,9 @@ package org.omilab.services.exampleservice.repo;
 
 import org.omilab.services.exampleservice.model.ForumPosting;
 import org.omilab.services.exampleservice.model.ForumThread;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,5 +14,8 @@ public interface ForumPostingRepository extends Repository<ForumPosting, Integer
     List<ForumPosting> findAllByForumUser_UserId(Integer forumUserId);
 
     ForumPosting save(ForumPosting forumPosting);
+
+    @Modifying
+    @Transactional
     ForumPosting deleteByPostingId(Integer postingId);
 }
